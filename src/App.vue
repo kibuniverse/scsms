b7600dbc8f66b5d8b2e3347a4b90728043399f4b
wt032320 <1476081727@qq.com>
2021-05-15 13:23:40 +0800

    init: vue-router and eslint

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
-<!--
- * @Author: your name
- * @Date: 2021-05-12 17:19:38
- * @LastEditTime: 2021-05-12 19:31:52
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \scsms\src\App.vue
--->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, reactive } from 'vue'
+export default defineComponent({
+  name: 'Main',
+  components: {}, 
+})
+</script>
+
+<style scoped>
+
+</style>

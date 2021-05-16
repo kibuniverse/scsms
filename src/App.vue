37e460ccb40ce76593926b7e30b8ef83be83bea8
kibuniverse <3050540005@qq.com>
2021-05-16 15:04:27 +0800

    feat: 买车页面

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+
 export default defineComponent({
-  name: 'App',
-  components: {
-  }
-})
+  name: "App",
+  components: {},
+});
 </script>
 
 <style>
-
 </style>

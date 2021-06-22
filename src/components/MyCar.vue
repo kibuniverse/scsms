46225127625385c9a76f2e9457741b13c06badfb
wt032320 <1476081727@qq.com>
2021-06-22 13:59:16 +0800

    feat: router

---
+++ src/components/MyCar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    我的车
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'MyCar',
+    components: {},
+    setup() {
+      
+    }
+  })
+</script>
+
+<style>
+
+</style>
